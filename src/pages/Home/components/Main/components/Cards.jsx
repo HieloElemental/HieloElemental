c4a46439d4e99613a6de12262da798f91bfb4991
Hielo Elemental <hieloelemental@gmail.com>
2024-08-01 23:03:03 -0500

    feat(Home projects section): :sparkles: Add portofolio project to Home and fix Educational background Card content

--- src/pages/Home/components/Main/components/Cards.jsx
+++ src/pages/Home/components/Main/components/Cards.jsx
@@ -37,17 +37,14 @@ const Cards = () => {
         type='third'
         maxWidth='max-w-xl'
       >
-        As a Systems Engineering student and self-taught full-stack developer, I
-        bring 5 years of hands-on experience in crafting dynamic and responsive
-        web applications. Specializing in React and Tailwind CSS for the
-        frontend, and Express.js for the backend, I have a proven track record
-        of building efficient and scalable solutions. My projects, including a
-        comprehensive institutional web management system, highlight my ability
-        to merge technical prowess with creative problem-solving. I am driven by
-        a relentless passion for coding, an insatiable thirst for learning, and
-        a unique ability to adapt and thrive in both solo and team environments.
-        Eager to bring my skills to a forward-thinking company, I am ready to
-        contribute and grow within a collaborative and innovative team.
+        I have a background in <b>PHP</b> from earlier studies and currently, I
+        am pursuing a <b>professional route</b> to obtain a diploma in{" "}
+        <b>Full-Stack Development with JavaScript</b>, besides as being a{" "}
+        <b>Systems Engineering</b> student at the prestigious{" "}
+        <b>Universidad Nacional de Colombia</b>. My knowledge spans a broad
+        spectrum of technologies, including an intermediate understanding of{" "}
+        <b>MySQL</b> and PostgreSQL, as well as the basics of <b>C++</b> and{" "}
+        <b>Java</b>.
       </Card>
     </div>
   );
--- src/pages/Home/components/Main/components/ProjectsSection.jsx
+++ src/pages/Home/components/Main/components/ProjectsSection.jsx
@@ -1,5 +1,6 @@
 import notePlatformHomeImage from "../../../../../assets/note-platform-home.jpg";
 import romeralEstereoHomeImage from "../../../../../assets/romeral-estereo-home.png";
+import hieloelementalPortfolioHomeImage from "../../../../../assets/hieloelemental-portfolio-home.png";
 import Project from "../../../../../components/Project";
 
 const projects = [
@@ -18,6 +19,14 @@ const projects = [
       "A radio website developed in php, using a React-like syntax, a bit of retro look, and a live audio transmision, This site has all the information of the radio and also has the song schedule for knowing when to hear and listen to your favorite genere",
     technologies: ["PHP", "HTML", "CSS"],
     link: "https://www.romeralestereo.com",
+  },
+  {
+    image: hieloelementalPortfolioHomeImage,
+    title: "HieloElemental Portfolio",
+    description:
+      "This webside was made by me from scratch using React and Tailwind CSS, it's a showcase of my skills, experiences, and projects. This website is made as a portfolio of my profile, made to show what things can I do. It also has a comunication with the github API to retrieve all my recent repositories and a blog where I will put my personal learning.",
+    technologies: ["React", "Tailwind CSS"],
+    link: "https://www.hieloelemental.com",
   },
 ];
 
